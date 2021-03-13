552d14a52117a970922479b1ff1502defd77531d
Paulo Sérgio <ps131296@gmail.com>
2021-03-13 10:28:03 -0300

    add default tags to layout instead of nuxt config

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,5 +12,30 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'DefaultLayout',
+  head() {
+    return {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'title',
+          name: 'title',
+          content: 'pssr.dev',
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Personal blog for sharing my challenges in development.',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: '/logo.png',
+        },
+        { property: 'og:image:width', content: '740' },
+        { property: 'og:image:height', content: '300' },
+      ],
+    }
+  },
 })
 </script>
