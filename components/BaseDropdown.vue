044e1df5009c235d94da3a5ac2ce1dd74d2b98a7
Paulo Sérgio <ps131296@gmail.com>
2021-03-12 23:31:53 -0300

    add dropdown

---
+++ components/BaseDropdown.vue
@@ -0,0 +1,84 @@
+<template>
+  <button class="text-left" @click="toggleVisible()">
+    <base-icon
+      v-if="!visible"
+      class="w-8 h-8 fill-current text-gray-200"
+      icon="hamburguer"
+    />
+
+    <base-icon v-else class="w-8 h-8 fill-current text-gray-200" icon="close" />
+
+    <div v-if="visible" class="absolute right-0 bg-indigo-500">
+      <div class="w-screen bg-indigo-500 mt-4 px-2 py-2">
+        <router-link
+          to="/"
+          class="text-lg block p-2 rounded text-white font-semibold hover:bg-indigo-400"
+        >
+          All posts
+        </router-link>
+
+        <router-link
+          to="/about"
+          class="text-lg block p-2 rounded text-white font-semibold hover:bg-indigo-400"
+        >
+          About
+        </router-link>
+
+        <a
+          download="curriculo_pssr.pdf"
+          href="/curriculo_pssr.pdf"
+          class="text-lg block p-2 rounded text-white font-semibold hover:bg-indigo-400"
+        >
+          Resume
+        </a>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/pssr"
+        >
+          <base-icon
+            class="w-8 h-8 mx-1 my-2 fill-current text-white rounded hover:bg-indigo-400"
+            icon="linkedin"
+          />
+        </a>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.github.com/ps1312"
+        >
+          <base-icon
+            class="w-8 h-8 mx-1 my-2 fill-current text-white rounded hover:bg-indigo-400"
+            icon="github"
+          />
+        </a>
+      </div>
+    </div>
+
+    <div
+      v-if="visible"
+      class="opacity-30 fixed w-screen h-screen top-0 mt-16 left-0 bg-gray-600 -z-10"
+    />
+  </button>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon'
+export default {
+  name: 'BaseDropdown',
+  components: {
+    BaseIcon,
+  },
+  data() {
+    return {
+      visible: false,
+    }
+  },
+  methods: {
+    toggleVisible() {
+      this.visible = !this.visible
+    },
+  },
+}
+</script>
---
+++ test/BaseDropdown.spec.js
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils'
+import BaseDropdown from '@/components/BaseDropdown'
+
+describe('BaseDropdown.vue', () => {
+  test('dropdown content is visible correctly', async() => {
+    const sut = makeSUT()
+
+    expect(sut.vm.visible).toBe(false)
+
+    await simulate(sut, 'mouseenter')
+    expect(sut.vm.visible).toBe(true)
+
+    await simulate(sut, 'mouseleave')
+    expect(sut.vm.visible).toBe(false)
+  })
+})
+
+function makeSUT(buttonTitle = 'default title') {
+  return mount(BaseDropdown, {
+    propsData: {
+      buttonTitle,
+    },
+  })
+}
+
+async function simulate(element, trigger) {
+  await element.trigger(trigger)
+}
